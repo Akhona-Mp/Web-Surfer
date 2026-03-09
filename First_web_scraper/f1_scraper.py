befbba969a5c59e17a358aa47948426c9559fe6c
AkhonaMphulo <akhonamphulo771@gmail.com>
2026-03-09 14:34:04 +0200

    Add padas to improve data capturing & scraping through meta data imbeded with in the js pages and added a delay time to reduce the amout of requests sent all at once.

--- First_web_scraper/f1_scraper.py
+++ First_web_scraper/f1_scraper.py
@@ -1,6 +1,9 @@
 import requests
 import csv
+import pandas as pd
 from bs4 import BeautifulSoup
+import time
+
 
 url = "https://www.motogp.com/en/riders/motogp"
 response = requests.get(url)
@@ -8,11 +11,8 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 riders = soup.find_all("a",class_="rider-list__rider")
+riders_data = []
 
-print(len(riders))
-
-# for rider in riders:
-#     print(rider.text)
 
 for rider in riders:
     # Check MotoGP class
@@ -32,28 +32,61 @@ for rider in riders:
     # Profile Link
     profile_link = f"https://www.motogp.com{rider.get('href')}" 
 
-    print(">",rider_name, "|", team_name, "|", country_name, "|", profile_link)
+    # This helps see which page is being scraped
+    print(f"Requesting profile page: {profile_link}")
+    profile_response = requests.get(profile_link)
+
+    print(f"Status code: {profile_response.status_code}")
+    print(profile_response.text[:300])
+    print("-" * 60)
+
+    profile_soup = BeautifulSoup(profile_response.text, "html.parser")
+
+    # Visiting each riders profile page 
+    first_name = profile_soup.find("meta", {"name": "rider-name"})
+    last_name = profile_soup.find("meta", {"name": "rider-lastname"})
+
+    if first_name and last_name:
+        full_name = first_name["content"] + " " + last_name["content"]
+    else:
+        full_name = name
+
+    riders_data.append({
+        "name": full_name,
+        "team": team_name,
+        "country": country_name,
+        "profile_link": profile_link
+    })
+    time.sleep(5)
+
+# # Open CSV file in write mode
+# with open("motogp_riders.csv", mode="w", newline="", encoding="utf-8") as file:
+#     writer = csv.writer(file)
+
+#     # Header row
+#     writer.writerow(["Name", "Team", "Country", "Profile Link"])
+
+#     # Loop through filtered riders
+#     for rider in riders:
+#         if rider.get("data-rider-category-id") != "737ab122-76e1-4081-bedb-334caaa18c70":
+#             continue
+
+#         # Extract details
+#         name = " ".join([span.text.strip() for span in rider.find("div", class_="rider-list__info-name").find_all("span")])
+#         team = rider.find("span", class_="rider-list__details-team").text.strip()
+#         country = rider.find("span", class_="rider-list__details-country").text.strip()
+#         profile = f"https://www.motogp.com{rider.get('href')}"
+
+#         # Write row
+#         writer.writerow([name, team, country, profile])
 
 
-# Open CSV file in write mode
-with open("motogp_riders.csv", mode="w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
+# print("CSV saved successfully ✅")
 
-    # Header row
-    writer.writerow(["Name", "Team", "Country", "Profile Link"])
+# Convert to dataframe
+df = pd.DataFrame(riders_data)
 
-    # Loop through filtered riders
-    for rider in riders:
-        if rider.get("data-rider-category-id") != "737ab122-76e1-4081-bedb-334caaa18c70":
-            continue
+# Save CSV
+df.to_csv("motogp_riders_full.csv", index=False)
 
-        # Extract details
-        name = " ".join([span.text.strip() for span in rider.find("div", class_="rider-list__info-name").find_all("span")])
-        team = rider.find("span", class_="rider-list__details-team").text.strip()
-        country = rider.find("span", class_="rider-list__details-country").text.strip()
-        profile = f"https://www.motogp.com{rider.get('href')}"
-
-        # Write row
-        writer.writerow([name, team, country, profile])
-
-print("CSV saved successfully ✅")
+print("Scraping complete. CSV saved.✅")
